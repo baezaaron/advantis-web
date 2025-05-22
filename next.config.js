4d978be973f5d15c5b160ddfafd7936170d19ca5
baezaaron <baezaaron@gmail.com>
2025-05-22 02:32:44 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     // Disable ESLint during development to focus on fixing the styles
     ignoreDuringBuilds: true,
